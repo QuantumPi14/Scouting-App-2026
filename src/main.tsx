1a1382af70d2f2ccd53b3acfb1faec982e76ce19
Georgio <quantumpi@icloud.com>
2026-03-11 16:55:18 -0400

    Added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { inject } from '@vercel/analytics'
 import { AppProvider } from './context'
 import App from './App'
 import './index.css'
+
+// Initialize Vercel Web Analytics (runs once on client)
+inject()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
